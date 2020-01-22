8b460b5373b36e056b33d6e93304f512e7e6d29c
gtsayan <48355689+gtsayan@users.noreply.github.com>
2020-01-22 14:45:25 +0300

    register

---
+++ src/blocks/modules/testimonials/testimonials.js
@@ -0,0 +1,13 @@
+modules.define('testimonials', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
